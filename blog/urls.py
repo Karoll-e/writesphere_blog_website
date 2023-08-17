b7276d85b6a506e9595f6d4eef9daa6cb166acf6
Karoll Escalante <karollescalante02@gmail.com>
2023-08-17 11:48:12 -0500

    Reorganized URLs: Moved user app URLs to users/urls.py and included in project's urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path("post/new/", PostCreateView.as_view(), name ='post-create'),
     path("post/<int:pk>/update", PostUpdateView.as_view(), name ='post-update'),
     path("post/<int:pk>/delete", PostDeleteView.as_view(), name ='post-delete'),
-    path("about/", views.about, name='blog-about'),
+    path("about/", views.about, name='blog-about')
 ]

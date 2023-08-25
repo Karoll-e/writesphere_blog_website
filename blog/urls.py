0bee9be0f6bdcad820b6ed02c44921d4b345130a
Karoll Escalante <karollescalante02@gmail.com>
2023-08-24 20:17:34 -0500

    Profile template design update v1.1

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", PostListView.as_view(), name='blog-home'),
-    path("<str:username>", UserPostListView.as_view(), name='user-posts'),
+    path("<str:username>", UserPostListView.as_view(), name='profile'),
     path("post/<int:pk>/", PostDetailView.as_view(), name ='post-detail'),
     path("post/new/", PostCreateView.as_view(), name ='post-create'),
     path("post/<int:pk>/update", PostUpdateView.as_view(), name ='post-update'),
--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("register/", users_views.register, name='register'),
-    path('profile/', users_views.profile, name='profile'),
+    path('settings/', users_views.profile, name='settings'),
     path("login/", auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path("logout/", auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('password-reset/',

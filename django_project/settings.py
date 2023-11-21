9075ea715857577ca0e7324187aa9b54bbe4476b
Karoll Escalante <karollescalante02@gmail.com>
2023-11-21 00:35:47 -0500

    changed enviroment variable method

--- django_project/settings.py
+++ django_project/settings.py
@@ -94,8 +94,8 @@ DATABASES = {
     }
 }
 
-database_url = os.environ.get("DATABASE_URL")
-DATABASES["default"] = dj_database_url.parse(database_url)
+# database_url = os.environ.get("DATABASE_URL")
+# DATABASES["default"] = dj_database_url.parse(database_url)
 
 
 # Password validation
